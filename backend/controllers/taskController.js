8d9dc0d40b423dd31fb4b5504f1a936e59e6ac17
Innoviziatech <git.innovizia@gmail.com>
2022-03-24 17:52:28 +0530

    deleted all folders

--- backend/controllers/taskController.js
+++
@@ -1,8 +0,0 @@
-const Task = require("../models/taskModel");
-const factory = require("./handleFactory");
-
-exports.getAllTasks = factory.getAll(Task);
-exports.getTask = factory.getOne(Task);
-exports.createTask = factory.createOne(Task);
-exports.updateTask = factory.updateOne(Task);
-exports.deleteTask = factory.deleteOne(Task);
